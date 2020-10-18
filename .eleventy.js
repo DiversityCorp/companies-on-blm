d2fa1df1a68d42359eddd1e874cf744f9370ccb1
Kelsey Huse <kelseyhuse30@gmail.com>
2020-10-18 17:21:25 -0500

    test2

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,10 @@ const dateFns = require("date-fns");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("dateReadable", (date) => dateFns.format(date, "dd MMM yyyy"));
+  eleventyConfig.addPassthroughCopy("images");
 
   return {
-    templateFormats: ["njk", "md", "css", "png"],
+    templateFormats: ["njk", "md", "css"],
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
   };
